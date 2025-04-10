a14f93abc60747442d88a48e7414c9a91716ac55
Hernan Marano <herchugm@gmail.com>
2025-04-10 16:05:37 -0300

    mas cambios

---
+++ informe_semana_04.md
@@ -0,0 +1,8 @@
+# Reporte
+- El parque con más cantidad de árboles es: **S/D**.
+- El parque con los árboles más altos en promedio es: **INFANTE DON ENRIQUE EL NAVEGANTE""**.
+- El parques con más variedad de especies es: **S/D**.
+- La especie mas popular es: **Eucalyptus sp.**.
+- La razón entre especies exóticas y autóctonas: **1.92**.
+
+Creado: 2025-04-10 16:01:53
--- semana_04.py
+++ semana_04.py
@@ -1,17 +1,29 @@
 import csv
 import statistics
-import math
+import datetime
+from pdb import set_trace as bp
 # long,lat,id_arbol,altura_tot,diametro,inclinacio,id_especie,nombre_com,nombre_cie,tipo_folla,espacio_ve,ubicacion,nombre_fam,nombre_gen,origen,coord_x,coord_y
 
 nombre_archivo = 'arbolado-en-espacios-verdes.csv'
 
+def writeFile(path, content):
+    with open(path,"w+", encoding="utf-8") as file:
+        file.write(content)
+
 def groupBy(l, keyPredicate):
     groupBy = {}
-    for a in l:
-        key = keyPredicate(a, l[a])
-        if key not in groupBy:
-            groupBy[key] = []
-        groupBy[key].append(l[a])
+    if isinstance(l, dict):
+        for a in l:
+            key = keyPredicate(a, l[a])
+            if key not in groupBy:
+                groupBy[key] = []
+            groupBy[key].append(l[a])
+    elif isinstance(l, list):
+        for i in range(len(l)):
+            key = keyPredicate(i, l[i])
+            if key not in groupBy:
+                groupBy[key] = []
+            groupBy[key].append(l[i])
     return groupBy
 
 def max(l, predicate):
@@ -92,31 +104,34 @@ La especie que más ejemplares tiene en la ciudad.
 La razón entre especies exóticas y autóctonas.
 '''
 
-def getParquesMasForestados(arbolesPorParque):
-    tally = [(key, len(arbolesPorParque[key])) for key in arbolesPorParque]
+def pickMax(tally):
     if len(tally) == 0:
         return None
     tally.sort(key= lambda a: a[1], reverse=True)
     max = tally[0][1]
     return [e[0] for e in tally if e[1] >= max]
+
+
+def getParquesMasForestados(arbolesPorParque):
+    tally = [(key, len(arbolesPorParque[key])) for key in arbolesPorParque]
+    return pickMax(tally)
 
 
 def getParquesConArbolesMasAltosEnPromedio(arbolesPorParque):
     tally = [(key, statistics.mean([int(a["altura_tot"]) for a in arbolesPorParque[key]])) for key in arbolesPorParque]
-    if len(tally) == 0:
-        return None
-    tally.sort(key= lambda a: a[1], reverse=True)
-    max = tally[0][1]
-    return [e[0] for e in tally if e[1] >= max]
+    return pickMax(tally)
+
+
+def getParquesConMasVariedadEspecies(arbolesPorParque):
+    tally = [(key, len([b for b in groupBy([a["nombre_cie"] for a in arbolesPorParque[key]], lambda k, v: v) ])) for key in arbolesPorParque]
+    return pickMax(tally)
 
 
 def getEspecieMasPopular(arboles):
     arbolesPorEspecie = groupBy(arboles, lambda k, v: v["nombre_cie"])
     tally =[(key, len(arbolesPorEspecie[key])) for key in arbolesPorEspecie]
-    if len(tally) == 0:
-        return None
-    tally.sort(key= lambda a: a[1], reverse=True)
-    return tally[0][0]
+    return pickMax(tally)
+
 
 def getRazonExoticosAutoctonos(arboles):
     arbolesPorOrigen = groupBy(arboles, lambda k, v: v["origen"])
@@ -128,12 +143,14 @@ def getRazonExoticosAutoctonos(arboles):
         raise "Cantidad autoctonos es 0."
     return  cant_exoticos / cant_autoctonos
 
+
 def informe():
     arboles = arboles_parque(nombre_archivo,None)
     arbolesPorParque = groupBy(arboles, lambda k, v: v["espacio_ve"] )
     return {
         "parquesMasForestados": getParquesMasForestados(arbolesPorParque),
         "parquesConArbolesMasAltos": getParquesConArbolesMasAltosEnPromedio(arbolesPorParque),
+        "parquesConMasVariedadEspecies": getParquesConMasVariedadEspecies(arbolesPorParque),
         "especieMasPopular": getEspecieMasPopular(arboles),
         "razonExoticosAutoctonos": getRazonExoticosAutoctonos(arboles)
     }
@@ -150,4 +167,6 @@ if __name__ == '__main__':
     promedioAltura = altura_promedio(nombre_parque, especie)
     print(f"La altura promedio de los árboles de la especie '{especie}' en el parque '{nombre_parque}' es {round(promedioAltura,2)}.")
     _informe = informe()
+    informe_str =f"# Reporte\n- {"El parque" if len(_informe["parquesMasForestados"]) <= 1 else "Los parques"} con más cantidad de árboles {"es" if len(_informe["parquesMasForestados"]) <= 1 else "son"}: **{" ".join(_informe["parquesMasForestados"])}**.\n- {"El parque" if len(_informe["parquesConArbolesMasAltos"]) <= 1 else "Los parques"} con los árboles más altos en promedio {"es" if len(_informe["parquesConArbolesMasAltos"]) <= 1 else "son"}: **{" ".join(_informe["parquesConArbolesMasAltos"])}**.\n- {"El" if len(_informe["parquesConMasVariedadEspecies"]) <= 1 else "Los"} parques con más variedad de especies {"es" if len(_informe["parquesConMasVariedadEspecies"]) <= 1 else "son"}: **{" ".join(_informe["parquesConMasVariedadEspecies"])}**.\n- {"La especie mas popular es" if len(_informe["especieMasPopular"]) <= 1 else "Las especies mas populares son"}: **{" ".join(_informe["especieMasPopular"])}**.\n- La razón entre especies exóticas y autóctonas: **{round(_informe["razonExoticosAutoctonos"],2)}**.\n\nCreado: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}"
     print(_informe)
+    writeFile("informe_semana_04.md", informe_str)
